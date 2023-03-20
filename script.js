8fdfb41a9da31acbdf17ab592fcd2776ec46a746
Rajat Deb <109984521+d-rajat28@users.noreply.github.com>
2023-03-20 12:49:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ function calculateMinCost() {
     inputArr[i] = Number(inputArr[i]);
   }
   var cost = 0;
-
+	inputArr.sort(function (a,b) {return a-b;});
   while (inputArr.length > 1) {
     var newRope = inputArr[0] + inputArr[1];
     cost += newRope;
     inputArr.splice(0, 2);
     inputArr.push(newRope);
+	  inputArr.sort(function (a,b) {return a-b;});
   }
   document.querySelector("#result").textContent = cost;   
 }  
